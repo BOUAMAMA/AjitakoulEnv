dd9ddf5107fd1c4bc60fa69391cee0fb340e814d
Imane <im.bouamama@gmail.com>
2018-07-11 17:56:01 +0900

    Update Django for whitenoise static

--- ajitakoul/production.py
+++ ajitakoul/production.py
@@ -143,7 +143,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
