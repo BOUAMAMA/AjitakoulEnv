f53b4938eb995477b6b8aab6e2a2bfbc23b408aa
Imane <im.bouamama@gmail.com>
2018-07-11 17:01:24 +0900

    Update Django for whitenoise static

--- ajitakoul/production.py
+++ ajitakoul/production.py
@@ -15,8 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_ROOT = normpath(join(BASE_DIR, 'assets'))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -27,7 +27,7 @@ STATICFILES_DIRS = (
     normpath(join(BASE_DIR, 'static')),
     normpath(join(BASE_DIR, 'upload')),
 )
-    #os.path.join(BASE_DIR, 'AjitakoulEnv/static'),
+    os.path.join(BASE_DIR, 'AjitakoulEnv/static'),
 )
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root') 
