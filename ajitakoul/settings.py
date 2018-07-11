0e8eb2faf8c5b9b1c83b58fb151ca42d59fc426a
Imane <im.bouamama@gmail.com>
2018-07-11 17:03:51 +0900

    Update Django for whitenoise static

--- ajitakoul/settings.py
+++ ajitakoul/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
