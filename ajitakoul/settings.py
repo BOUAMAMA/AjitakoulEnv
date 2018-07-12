038b543534965a848fe6b9e2a010510743251c9b
Imane <im.bouamama@gmail.com>
2018-07-12 12:57:34 +0900

    Initial Heroku commit

--- ajitakoul/settings.py
+++ ajitakoul/settings.py
@@ -139,8 +139,10 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 if DEBUG:
     MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+  
+    #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
     STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
+    #os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
         )
