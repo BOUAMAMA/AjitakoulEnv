254528a1ba83dae5a3909a9244513336478256b8
Imane <im.bouamama@gmail.com>
2018-07-13 14:34:07 +0900

    Initial Heroku commit

--- ajitakoul/wsgi.py
+++ ajitakoul/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ajitakoul.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ajitakoul.settings.dev")
 
 application = get_wsgi_application()
 
